dd55293a28825ccbcb0d80d6cc2592ddf1f32497
Saif Ali <kattan.saifali@gmail.com>
2023-10-07 20:47:56 +0200

    exercis completed

---
+++ exercis_pratique_python/exercis9.py
@@ -0,0 +1,14 @@
+
+#  Nombres impairs inclus dans un intervalle
+
+# for loop in range(10,21):
+#     if loop  % 2  == 1: 
+#         print("Loop: ",loop)
+
+
+# 
+impair = 10
+while impair <= 20:
+    if impair %2 == 1:
+     print('Impairs:',impair )
+    impair += 1
