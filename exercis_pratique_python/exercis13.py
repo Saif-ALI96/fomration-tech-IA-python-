efada9c41ab22a62ca738133d2086efd1f78d42c
Saif Ali <kattan.saifali@gmail.com>
2023-10-08 18:29:36 +0200

    exercis completed

---
+++ exercis_pratique_python/exercis13.py
@@ -0,0 +1,14 @@
+
+# Nombres d'occurrences d'un élément 
+
+'''
+The count() method returns the number of elements with the specified value.
+'''
+
+L = [3,2,2,1,9,1,2,3,7]
+
+nombre_compter = L.count(1)
+
+print (nombre_compter)
+#  sortie ----> 2
+
