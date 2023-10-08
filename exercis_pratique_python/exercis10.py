933e78774c1a3ed7a7b8d59ee746007507b791fb
Saif Ali <kattan.saifali@gmail.com>
2023-10-08 18:28:49 +0200

    exercis completed

---
+++ exercis_pratique_python/exercis10.py
@@ -0,0 +1,24 @@
+
+#  Liste de compréhension
+'''
+liste = []
+liste.append(1)
+liste.append(2)
+liste.append(3)
+liste.append(4)
+liste.append(5)
+liste.append(6)
+liste.append(7)
+liste.append(8)
+liste.append(9)
+liste.append(10)
+
+print(liste)'''
+
+liste = []
+
+for nombres in range(1,11):
+    liste.append(nombres)
+print(liste)
+
+#  sortie --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
