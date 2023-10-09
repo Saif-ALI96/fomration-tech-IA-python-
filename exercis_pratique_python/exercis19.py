faf737055cc25f5dd00548bcb320e9cc013b5bc6
Saif Ali <kattan.saifali@gmail.com>
2023-10-09 11:20:39 +0200

    exercis completed

---
+++ exercis_pratique_python/exercis19.py
@@ -0,0 +1,14 @@
+
+#  Inverser une chaine 
+
+ch = 'Bonjour tout le monde'
+inverser_chaine_de_caratere = ''.join(reversed(ch))
+print (f" le pharse est la suivante : {inverser_chaine_de_caratere}")
+
+
+#  methode join() , chaîne_resultat = séparateur.join(séquence)
+'''
+`séparateur` est la chaîne de caractères que vous souhaitez insérer entre chaque élément de la séquence.
+`séquence` est la séquence dont vous voulez joindre les éléments. 
+Cela peut être une liste, un tuple, ou une autre séquence itérable contenant des chaînes de caractères.
+'''
