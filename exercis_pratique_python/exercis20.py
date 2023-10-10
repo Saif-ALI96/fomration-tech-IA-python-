71f2796e34751dbab3dcfbcf9f387df6d3fba1da
Saif Ali <kattan.saifali@gmail.com>
2023-10-10 09:48:29 +0200

    terminer l'exercis

---
+++ exercis_pratique_python/exercis20.py
@@ -0,0 +1,15 @@
+
+#  Les valeurs d'un dictionnaire 
+
+dict_fruits = {'Pomme':3 , 'Banane': 7, 'Kiwi':1}
+
+cle_afficher = ['Pomme', 'Banane']
+for i in cle_afficher:
+    if i in dict_fruits:
+        # 1- 
+        # print(dict_fruits)  # ----> {'Pomme': 3, 'Banane': 7, 'Kiwi': 1} {'Pomme': 3, 'Banane': 7, 'Kiwi': 1}
+        # il repete deux fois parce que la liste contient deux elements
+        # 2- 
+        # print(key) # ----> Pomme Banane , ca va afficher les deux cles 
+        # 3-
+        print(dict_fruits[i]) # ----> ca va afficher les valeurs de cles "Pomme" et "Banane" qui sont le 3 et 7
