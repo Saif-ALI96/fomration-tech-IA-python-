c9c569558eecddcbb01cc6beda2c36d8b064ff18
Saif Ali <kattan.saifali@gmail.com>
2023-10-10 09:50:01 +0200

    terminer l'exercis

---
+++ exercis_pratique_python/exercis22.py
@@ -0,0 +1,13 @@
+
+# Utilisation de la méthode format()
+
+nombre_decimal = 187.632587
+# print('la reponse est : %.2f' %((nombre_decimal))) # sorite -----> la reponse est : 187.63
+
+print('la reponse est : {0:.2f}' .format((nombre_decimal))) # sortie ----> la reponse est : 187.63
+
+
+
+#  le %s ---- >  c'est pour le type STRING
+#  le %d ---- >  c'est pour le type NUMBERS
+#  le %f ---- >  c'est pour le type FLOAT
