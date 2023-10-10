683ea5b5d44e1af36c770dcb23ec8d08de890707
Saif Ali <kattan.saifali@gmail.com>
2023-10-10 09:51:32 +0200

    terminer l'exercis

---
+++ exercis_pratique_python/exercis23.py
@@ -0,0 +1,11 @@
+
+
+#  Utilisation de la méthode format()
+
+monNom = 'Julien' 
+age = 32
+langage = 'Python'
+
+print('je m\'appell {} et j\'ai {} ans. J\'apprends le langage {}'.format(monNom,age,langage))
+
+# sortie ----> je m'appell Julien et j'ai 32 ans. J'apprends le langage Python
