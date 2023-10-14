71bc4e72104e901e649f138e59b7df921feef884
Saif Ali <kattan.saifali@gmail.com>
2023-10-14 21:12:44 +0200

    exercis completed

---
+++ exercis_pratique_python/exercis30.py
@@ -0,0 +1,13 @@
+
+# Générer aléatoirement un nombre 
+# Écrire un programme qui permet de renvoyer aléatoirement un nombre entre 20 et 30.
+
+import random
+
+print(random.randint(20,30))
+
+# print (f'nombre aléatoire est : {random.choice(range(20,30))}') 
+
+
+# La fonction random.choice() fait partie du module random en Python et est utilisée pour 
+# sélectionner de manière aléatoire un élément d'une séquence (comme une liste, un tuple ou une chaîne de caractères). 
