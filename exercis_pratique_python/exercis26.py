4b181de04e0fad406b669a0037638957ec07924f
Saif Ali <kattan.saifali@gmail.com>
2023-11-02 23:48:33 +0100

    modification

--- exercis_pratique_python/exercis26.py
+++ exercis_pratique_python/exercis26.py
@@ -1,6 +1,6 @@
-
+''''
 #  Écrire un programme qui permet de supprimer un élément d'une liste. Considérons la liste L = [1,2,3,4,5]
-# et que nous voulons supprimer le nombre 1.
+# et que nous voulons supprimer le nombre 1.'''
 
 L = [1,2,3,4,5]
 L.remove(1)
