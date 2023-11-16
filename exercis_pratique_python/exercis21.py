554f7d36d96f8ff4b6d4f42029c1d0b2c5ee6268
Saif Ali <kattan.saifali@gmail.com>
2023-11-16 22:24:20 +0100

    modification

--- exercis_pratique_python/exercis21.py
+++ exercis_pratique_python/exercis21.py
@@ -2,14 +2,14 @@
 
 dicte = {'Pomme':15,'Banane':8,'Fraise':12,'Kiwi':9 ,'Peche':2}
 
-'''
-total = 0
-for i in dicte.values():
-    total += i
-    # print (total) 
 
-print (total)
-'''
+# total = 0
+# for i in dicte.values():
+#     total += i
+#     # print (total) 
+
+# print (total)
+
 
 #  avec la function sum()
 
